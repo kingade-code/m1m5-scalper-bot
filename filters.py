e4954988b5547169cb1ce65db2adb9434123fd56
kingade-code <kingade-code@users.noreply.github.com>
2026-08-26 15:15:06 +0100

    Trend filter: H1 -> M5

--- filters.py
+++ filters.py
@@ -53,7 +53,7 @@ def check_trend_filter(df, direction, symbol=None):
     import mt5_connector as mt5c
 
     ema_period = config.get_symbol_param(symbol, "TREND_EMA_PERIOD", config.TREND_EMA_PERIOD)
-    h1_df = mt5c.get_ohlc(symbol, 16385, ema_period + 50)
+    h1_df = mt5c.get_ohlc(symbol, 5, ema_period + 50)  # M5 timeframe
     if h1_df is None or len(h1_df) < ema_period + 10:
         return True
 
